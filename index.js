71db3a8f8621195a5566224f71d4fc6ad10edceb
ImmanuelN <141280765+ImmanuelN@users.noreply.github.com>
2024-11-21 10:15:56 +0200

    Added Login and Sign up functionality

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+require('dotenv').config();
 var Express = require("express");
 var MongoClient = require("mongodb").MongoClient;
 var cors = require("cors");
 const multer = require("multer");
+const bycript = require("bcryptjs");
+const jwt = require('jsonwebtoken')
 
 var app = Express();
 app.use(cors());
 app.use(Express.json()); // Add this to handle JSON requests
 
-var CONNECTION_STRING = "mongodb+srv://Imms:imms@cluster0.2ccip.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
-var DATABASENAME = "GameHub_Database";
+var CONNECTION_STRING = process.env.DB_CONNECTION_STRING; // Get from .env file
+var DATABASENAME = process.env.DB_NAME;
 var database;
 
 // Establish MongoDB connection and start the server
@@ -22,6 +25,94 @@ MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopolo
     app.listen(5038, () => {
         console.log("Server is running on port 5038");
     });
+});
+
+app.post("/api/gamehub/sign-up", async (request, response) => {
+    try {
+        const { username, email, password } = request.body;
+        const existingUser = await database.collection("Authentications").findOne({ email: email });
+
+        if (existingUser) {
+            return response.status(400).json("Email already exists. Please use a different email.");
+        }
+
+        // Hash the password using bcryptjs
+        const salt = await bycript.genSalt(10);
+        const hashedPassword = await bycript.hash(password, salt);
+
+        const data = {
+            username: username,
+            email: email,
+            password: hashedPassword,
+        };
+        
+        // Insert user data into the Authentications collection
+        const userData = await database.collection("Authentications").insertOne(data);
+        if (userData) {
+            console.log("auth data saved:", userData);
+            const user = {
+                authId: userData.insertedId,
+                username: username,
+                email: email,
+            }
+
+            // Insert user data into the Users collection
+            const userResponse = await database.collection("Users").insertOne(user);
+            console.log("user data saved:", userResponse);
+        }
+
+        // Return success response
+        response.json("Successfully Registered");
+
+    } catch (error) {
+        console.error("Error adding user:", error);
+        response.status(500).json("An error occurred while registering");
+    }
+});
+
+
+// Handle Logins
+app.post("/api/gamehub/login", async (request, response) => {
+    try {
+        const { email, password } = request.body;
+
+        // Check if the user exists in the database
+        const existingUser = await database.collection("Authentications").findOne({ email: email });
+        
+        if (existingUser) {
+            // Check if the password is correct
+            const isPasswordMatch = await bycript.compare(password, existingUser.password);
+            
+            if (isPasswordMatch) {
+                // Check if JWT_SECRET is defined
+                if (!process.env.JWT_SECRET) {
+                    console.error('JWT_SECRET is not defined');
+                    return response.status(500).json({ message: 'Server configuration error' });
+                }
+
+                // Generate JWT token
+                const token = jwt.sign(
+                    { id: existingUser._id}, // Use existingUser to get id and email
+                    process.env.JWT_SECRET, // JWT_SECRET loaded from .env
+                    { expiresIn: '720h' } // Token validity duration
+                );
+                
+                // Send response with token
+                return response.status(200).json({
+                    message: "Successfully Logged In",
+                    token: token
+                });
+            } else {
+                return response.status(401).json({ message: "Password is incorrect, Try Again" });
+            }
+        } else {
+            return response.status(404).json({ message: "Email does not exist" });
+        }
+    } catch (error) {
+        // Return error response for any internal server errors
+        console.error(error);  // Optional: log the error for debugging purposes
+        return response.status(500).json({ message: "Error Logging In" });
+    }
 });
 
 // Fetch all games
